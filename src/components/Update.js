040df1599d1c7f53525eaae634bf25c014944716
Linas987 <73585313+Linas987@users.noreply.github.com>
2023-10-09 14:10:36 +0300

    eslint

--- src/components/Update.js
+++ src/components/Update.js
@@ -19,7 +19,9 @@ function Update({props,setToken}){
     const { password, name, surname, email } = user;
 
     const emailValidation=(email)=>{
+      // eslint-disable-next-line
       const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      // eslint-disable-next-line
       if(email!="" && regex.test(email) === false){
           alert("email not valid");
           return false;
